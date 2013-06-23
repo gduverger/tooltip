552628c22f1183c99a6c3347befb3cfdfe66c45d
Georges Duverger <gduverger@ebay.com>
2013-06-23 19:36:41 -0400

    Add demo

--- jquery.tooltip.js
+++ jquery.tooltip.js
@@ -1,63 +1,52 @@
-(function( $ ){
-
-    //    left-top --- top-center ---- top-right
-    //           |                     |
-    // left-middle       target        right-middle [0]
-    //           |                     |
-    // bottom-left -- bottom-center -- right-bottom
-
-    var positions = {
-        "right-middle": function( targetDimension, tooltipDimension ) {
-            return { top: targetDimension.top + ( targetDimension.height / 2 ) - ( tooltipDimension.height / 2 ), left: targetDimension.left + targetDimension.width };
-        },
-        "right-bottom": function( targetDimension, tooltipDimension ) {
-            return { top: targetDimension.top + ( targetDimension.height / 2 ), left: targetDimension.left + targetDimension.width };
-        },
-        "bottom-center": function( targetDimension, tooltipDimension ) {
-            return { top: targetDimension.top + targetDimension.height, left: targetDimension.left + ( targetDimension.width / 2 ) - ( tooltipDimension.width / 2 ) };
-        },
-        "bottom-left": function( targetDimension, tooltipDimension ) {
-            return { top: targetDimension.top + ( targetDimension.height / 2 ), left: targetDimension.left - tooltipDimension.width };
-        },
-        "left-middle": function( targetDimension, tooltipDimension ) {
-            return { top: targetDimension.top + ( targetDimension.height / 2 ) - ( tooltipDimension.height / 2 ), left: targetDimension.left - tooltipDimension.width };
-        },
-        "left-top": function( targetDimension, tooltipDimension ) {
-            return { top: targetDimension.top + ( targetDimension.height / 2 ) - tooltipDimension.height, left: targetDimension.left - tooltipDimension.width };
-        },
-        "top-center": function( targetDimension, tooltipDimension ) {
-            return { top: targetDimension.top - tooltipDimension.height, left: targetDimension.left + ( targetDimension.width / 2 ) - ( tooltipDimension.width / 2 ) };
-        },
-        "top-right": function( targetDimension, tooltipDimension ) {
-            return { top: targetDimension.top + ( targetDimension.height / 2 ) - tooltipDimension.height, left: targetDimension.left + targetDimension.width };
-        }
-    };
+( function( $ ) {
 
     $.fn.extend({
         tooltip: function( options ) {
 
             var settings = $.extend( {}, {
-                selector: null,
-                showOn: "mouseover",
-                hideOn: "mouseout",
-                $tooltip: $( "#tooltip" ),
-                $showInside: null,
-                extraMargins: { top: 0, right: 0, bottom: 0, left: 0 },
-                preferredPositions: [ "right-middle", "right-bottom", "bottom-center", "bottom-left", "left-middle", "left-top", "top-center", "top-right" ],
-                beforeShowCallback: function( event, $target, $tooltip ) { return true; },
-                afterShowCallback: function( event, $target, $tooltip ) {},
-                beforeHideCallback: function( event, $target, $tooltip ) { return true; },
-                afterHideCallback: function( event, $target, $tooltip ) {},
-            }, options );
 
-            var boxOffset = settings.$showInside ? settings.$showInside.offset() || { top: 0, left: 0 } : null,
-                boxDimension = settings.$showInside ? {
-                    height: settings.$showInside.height(),
-                    width: settings.$showInside.width()
+                    /* ----------------------------- Available options ---------------------------- */
+
+                    $box: null,
+                    selector: null,
+                    hideTimeout: 100,
+                    showTimeout: 100,
+                    hideOn: "mouseout",
+                    showOn: "mouseover",
+                    canHoverTooltip: true,
+                    $tooltip: $( "#tooltip" ),
+                    extraMargins: {
+                        top: 0,
+                        right: 0,
+                        bottom: 0,
+                        left: 0
+                    },
+                    preferredPositions: [
+                        "right-middle",
+                        "right-bottom",
+                        "bottom-center",
+                        "bottom-left",
+                        "left-middle",
+                        "left-top",
+                        "top-center",
+                        "top-right"
+                    ],
+                    beforeShowCallback: function( event, $target, $tooltip ) { return true; },
+                    afterShowCallback: function( event, $target, $tooltip ) {},
+                    beforeHideCallback: function( event, $target, $tooltip ) { return true; },
+                    afterHideCallback: function( event, $target, $tooltip ) {},
+                }, options ),
+
+                /* -------------------------------------------------------------------------------- */
+
+                boxOffset = settings.$box ? settings.$box.offset() || { top: 0, left: 0 } : null,
+                boxDimension = settings.$box ? {
+                    height: settings.$box.height(),
+                    width: settings.$box.width()
                 } : null;
 
             this.each(function() {
-                var $target, tooltipDimension, targetOffset, targetDimension, tooltipClass, tooltipOffset, show;
+                var $target, forcedPosition, tooltipDimension, targetOffset, targetDimension, tooltipClass, tooltipOffset, isShown;
 
                 $( this ).on( settings.showOn, settings.selector, function( event ) {
                     $target = $( this );
@@ -79,43 +68,47 @@
                         height: $target.outerHeight() || parseInt( $target.attr("r") ) * 2 || 0
                     };
 
-                    tooltipClass = settings.preferredPositions[0];
+                    forcedPosition = $target.data( "tooltip-position" );
+                    tooltipClass = forcedPosition ? forcedPosition : settings.preferredPositions[ 0 ];
                     tooltipOffset = positions[ tooltipClass ]( targetDimension, tooltipDimension );
 
                     var i = 0;
-                    while ( !doesFitInWindowAndBox( tooltipOffset, tooltipDimension, settings.extraMargins, boxOffset, boxDimension ) && i < settings.preferredPositions.length ) {
+                    while ( !forcedPosition && !canShowInsideWindowAndBox( tooltipOffset, tooltipDimension, settings.extraMargins, boxOffset, boxDimension ) && i < settings.preferredPositions.length ) {
                         tooltipClass = settings.preferredPositions[ i++ ];
                         tooltipOffset = positions[ tooltipClass ]( targetDimension, tooltipDimension );
                     }
-                    // TODO fallback if none of the positions fit
 
-                    settings.$tooltip.css( tooltipOffset ).addClass( tooltipClass ).show();
-                    show = true;
-
-                    settings.afterShowCallback( event, $target, settings.$tooltip );
+                    isShown = true;
+                    setTimeout( function() {
+                        if ( isShown ) {
+                            settings.$tooltip.css( tooltipOffset ).addClass( tooltipClass ).show();
+                            settings.afterShowCallback( event, $target, settings.$tooltip );
+                        }
+                    }, settings.showTimeout );
 
                 }).on( settings.hideOn, settings.selector, function( event ) {
                     $target = $( this );
 
                     if ( !settings.beforeHideCallback( event, $target, settings.$tooltip ) ) return null;
-                    show = false;
+                    isShown = false;
                     setTimeout( function() {
-                        if ( !show ) {
+                        if ( !isShown ) {
                             settings.$tooltip.hide();
                             settings.afterHideCallback( event, $target, settings.$tooltip );
                         }
-                    }, 100 );
+                    }, settings.hideTimeout );
 
                 });
 
-                // HACK
-                settings.$tooltip.on( "mouseenter", function( event ) {
-                    show = true;
-                }).on( "mouseleave", function( event ) {
-                    show = false;
-                    settings.$tooltip.hide();
-                    settings.afterHideCallback( event, $target, settings.$tooltip );
-                });
+                if ( settings.canHoverTooltip ) {
+                    settings.$tooltip.on( "mouseenter", function( event ) {
+                        isShown = true;
+                    }).on( "mouseleave", function( event ) {
+                        isShown = false;
+                        settings.$tooltip.hide();
+                        settings.afterHideCallback( event, $target, settings.$tooltip );
+                    });
+                }
 
             });
 
@@ -123,7 +116,7 @@
         }
     });
 
-    var doesFitInWindowAndBox = function( tooltipOffset, tooltipDimension, extraMargins, boxOffset, boxDimension ) {
+    function canShowInsideWindowAndBox( tooltipOffset, tooltipDimension, extraMargins, boxOffset, boxDimension ) {
         var $window = $( window ),
             windowOffset = { top: $window.scrollTop(), left: $window.scrollLeft() },
             windowDimension = { height: $window.height(), width: $window.width() };
@@ -136,6 +129,67 @@
             && ( !boxOffset || tooltipOffset.left > boxOffset.left + extraMargins.right )
             && ( !boxOffset || !boxDimension || tooltipOffset.top + tooltipDimension.height < boxOffset.top + boxDimension.height - extraMargins.bottom )
             && ( !boxOffset || !boxDimension || tooltipOffset.left + tooltipDimension.width < boxOffset.left + boxDimension.width - extraMargins.right );
+    }
+
+    /* ----------------------------------- Positions ----------------------------------
+
+        left-top [5] --- top-center [6] ---- top-right [7]
+                   |                         |
+     left-middle [4]         target          right-middle [0]
+                   |                         |
+     bottom-left [3] -- bottom-center [2] -- right-bottom [1]
+
+    -------------------------------------------------------------------------------- */
+
+    var positions = {
+        "right-middle": function( targetDimension, tooltipDimension ) {
+            return {
+                top: targetDimension.top + ( targetDimension.height / 2 ) - ( tooltipDimension.height / 2 ),
+                left: targetDimension.left + targetDimension.width
+            };
+        },
+        "right-bottom": function( targetDimension, tooltipDimension ) {
+            return {
+                top: targetDimension.top + ( targetDimension.height / 2 ),
+                left: targetDimension.left + targetDimension.width
+            };
+        },
+        "bottom-center": function( targetDimension, tooltipDimension ) {
+            return {
+                top: targetDimension.top + targetDimension.height,
+                left: targetDimension.left + ( targetDimension.width / 2 ) - ( tooltipDimension.width / 2 )
+            };
+        },
+        "bottom-left": function( targetDimension, tooltipDimension ) {
+            return {
+                top: targetDimension.top + ( targetDimension.height / 2 ),
+                left: targetDimension.left - tooltipDimension.width
+            };
+        },
+        "left-middle": function( targetDimension, tooltipDimension ) {
+            return {
+                top: targetDimension.top + ( targetDimension.height / 2 ) - ( tooltipDimension.height / 2 ),
+                left: targetDimension.left - tooltipDimension.width
+            };
+        },
+        "left-top": function( targetDimension, tooltipDimension ) {
+            return {
+                top: targetDimension.top + ( targetDimension.height / 2 ) - tooltipDimension.height,
+                left: targetDimension.left - tooltipDimension.width
+            };
+        },
+        "top-center": function( targetDimension, tooltipDimension ) {
+            return {
+                top: targetDimension.top - tooltipDimension.height,
+                left: targetDimension.left + ( targetDimension.width / 2 ) - ( tooltipDimension.width / 2 )
+            };
+        },
+        "top-right": function( targetDimension, tooltipDimension ) {
+            return {
+                top: targetDimension.top + ( targetDimension.height / 2 ) - tooltipDimension.height,
+                left: targetDimension.left + targetDimension.width
+            };
+        }
     };
 
 })( jQuery );
